75e58ab0bb7c42d80deccb8ce2ea87501ed26cfb
Carlos Cardenas <cardenascarlos18@gmail.com>
2023-09-08 22:31:35 -0500

    components

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,61 +21,7 @@
     </div>
 
 
-    <style>
-        .client {
-            color: var(--Negro-PDT, #171D2A);
-            font-family: Montserrat;
-            font-size: 36px;
-            font-style: normal;
-            font-weight: 1000;
-            line-height: normal;
-            margin-top: 350px;
-            /* Agrega un margen superior para separar el texto del contenido */
-        }
-
-      
-
-        .circle-pdt {
-            transform: translate(-50%, -50%);
-            position: absolute;
-            /* Establece una posición absoluta para el contenido */
-            left: 50%;
-            /* Centra el contenido horizontalmente en el contenedor */
-
-            border-radius: 336px;
-            background: var(--Blanco-PDT, #FFF);
-            box-shadow: 12px 15px 16px 0px rgba(0, 0, 0, 0.25);
-            width: 336px;
-            height: 336px;
-            flex-shrink: 0;
-        }
-
-
-
-
-
-        @keyframes rotateTail {
-            0% {
-                transform: rotateY(0deg);
-            }
-
-            25% {
-                transform: rotateY(15deg);
-            }
-
-            50% {
-                transform: rotateY(0deg);
-            }
-
-            75% {
-                transform: rotateY(-15deg);
-            }
-
-            100% {
-                transform: rotateY(0deg);
-            }
-        }
-    </style>
+   
 
 
     
