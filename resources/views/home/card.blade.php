cc029325e877caf2b44ef1d0feedc5bc15aa328c
Carlos Cardenas <cardenascarlos18@gmail.com>
2023-09-08 22:30:12 -0500

    components

---
+++ resources/views/home/card.blade.php
@@ -0,0 +1,39 @@
+<div>
+<div class=" bg-black">
+        <div class="m">
+            <div class="container ">
+                <div class=" row row-cols-1 row-cols-md-4 g-4">
+                    <div class="col">
+                        <div class="rounded-circle">
+                            <img src="{{ asset('image/foto.jpeg') }}" class="rounded card-img-top" alt="...">
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="rounded">
+                            <img src="{{ asset('image/foto2.png') }}" class="rounded card-img-top" alt="...">
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="rounded">
+                            <img src="{{ asset('image/foto3.png') }}" class="rounded card-img-top" alt="...">
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="rounded-pill">
+                            <img src="{{ asset('image/foto4.png') }}" class="rounded card-img-top" alt="...">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+</div>
---
+++ resources/views/home/video.blade.php
@@ -0,0 +1,15 @@
+<div class="mt-5 ">
+                <div class="row row-cols-3">
+                    <div>
+                        <h2>{{ __('WE MAKE YOUR IDEAS COME TRUE') }}</h2> <br>
+
+                        <p class="text-black">
+                        {{__("We are developers of the first GDS for travel agencies with a focus on health tourism.")}} <br> <br>
+                        {{__("Providing technological solutions to streamline processes and in one place, finding all health and wellness services so that travel agencies can enter a new era of tourism with greater reach, a wider range of services, and, of course, increasing their profits.")}}
+                    </p>
+                    </div>
+
+                
+
+                </div>
+            </div>
