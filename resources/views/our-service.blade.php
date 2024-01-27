81e30b373fd71ed04e68f58b9f49cab8734d915e
Carlos Cardenas <ccardenas@bv.studio>
2024-01-27 10:42:28 -0500

    changes

--- resources/views/our-service.blade.php
+++ resources/views/our-service.blade.php
@@ -101,45 +101,71 @@
 
             <div id='content-services'>
                 <div class="row text-white serv container-services">
-                    <div class="col-4">
-                        <h3>Nuestros Servicios</h3>
+                    <div class="col-md-4">
+                        <h3>Desarrollo a tu medida:</h3>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            Transformamos ideas en soluciones digitales. 
+                            Desde aplicaciones móviles hasta plataformas 
+                            web, nuestro equipo de desarrolladores 
+                            personaliza cada línea de código para cumplir 
+                            con tus requerimientos específicos.
                         </p>
                     </div>
-                    <div class="col-4">
-                        <h3>Nuestros Servicios</h3>
+                    <div class="col-md-4">
+                        <h3>Innovación Tecnológica:</h3>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            En PDT, no solo seguimos las tendencias, 
+                            las creamos. Nuestros desarrolladores están 
+                            a la vanguardia de las últimas tecnologías 
+                            para asegurar que tu proyecto no solo sea 
+                            funcional, sino también revolucionario.
                         </p>
                     </div>
-                    <div class="col-4">
-                        <h3>Nuestros Servicios</h3>
+                    <div class="col-md-4">
+                        <h3>Experiencia Fluida de Usuario:</h3>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            Construimos software que no solo resuelve 
+                            problemas, sino que también cautiva. 
+                            Nuestros desarrolladores se enfocan 
+                            en crear experiencias de usuario intuitivas 
+                            y atractivas para dejar una impresión duradera.
                         </p>
                     </div>
 
                     <div class="container-service-separator"></div>
 
-                    <div class="col-4">
-                        <h3>Nuestros Servicios</h3>
+                    <div class="col-md-4">
+                        <h3>Eficiencia en el Desarrollo:</h3>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            El tiempo es valioso. Nuestro equipo de 
+                            desarrolladores está comprometido con la 
+                            entrega oportuna de proyectos sin sacrificar 
+                            la calidad. Eficiencia y excelencia van de la 
+                            mano en cada línea de código que escribimos.
                         </p>
                     </div>
-                    <div class="col-4">
-                        <h3>Nuestros Servicios</h3>
+                    <div class="col-md-4">
+                        <h3>Seguridad en cada Aspecto:</h3>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            La seguridad no es una opción, es una 
+                            prioridad. Nuestros desarrolladores 
+                            implementan protocolos de seguridad 
+                            avanzados en cada fase del desarrollo para 
+                            garantizar la protección de tus datos y la 
+                            confidencialidad de tu proyecto.
                         </p>
                     </div>
 
 
-                    <div class="col-4">
-                        <h3>Nuestros Servicios</h3>
+                    <div class="col-md-4">
+                        <h3>Soporte Continuo:</h3>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            No terminamos cuando el desarrollo lo hace. 
+                            Nuestro equipo de soporte técnico está 
+                            disponible para garantizar que tu software 
+                            funcione sin problemas. Actualizaciones, 
+                            mejoras y asistencia constante para asegurar 
+                            el éxito a largo plazo.
                         </p>
                     </div>
                 </div>
